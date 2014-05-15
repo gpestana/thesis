0293e10ddbfc67898c5ff1b2a40717b582699a2e
gpestana <g6pestana@gmail.com>
2014-05-15 16:09:26 +0300

    exp script done

--- exp.sh
+++ exp.sh
@@ -7,13 +7,13 @@
 
 #input:
 #	$1 config file name
-#	$2 estimate time to run
-#	$3 name for folder (optional)
+#	$2 name for folder (optional)
+# 
 
-dir="$(date +'%d.%H%M')"'_'$3
+dir="$(date +'%d.%H%M')"'_'$2
 
 mkdir -p out/$dir
-echo 'input: '$1' '$2' '$3 >> out/$dir/cmd 
+echo 'input: '$1' '$2'' >> out/$dir/cmd 
 
 #top
 top -d 1 -b > out/$dir/top.log &
@@ -23,22 +23,39 @@ echo 'top running with '$pid_top
 sudo perf record -a -o out/$dir/perf_record.log &
 pid_perf=$!
 echo 'perf record running with '$pid_perf
-#perf stat
-sudo perf stat -a -r $2 sleep 1 &
 
 # >baseline
 #  >CPU
-stress --cpu 4 --timeout $2;
+#stress --cpu 4 --timeout $3 &
 #  >mem
-#stress --vm 4 --timeout $2
+#stress --vm 4 --timeout $3 &
 
 # >experiment 
-echo '-----------'
-time cmsRun $1;
-echo '-----------'
+#echo '-----------'
+#time cmsRun $1 &
+#echo '-----------'
+
+#new perf stat
+stress --cpu 1 --timeout $3 &
+track=$!
+
+while true
+do
+	if ps -p $track > /dev/null 2>&1; then
+		ps v >> out/$dir/ps.out
+		(sudo perf stat -a sleep 1) >>  out/$dir/perf_stat.out 2>&1
+	else
+		echo 'master process is done'
+		break
+	fi
+	sleep 5
+done
 
 sudo kill -2 $pid_top;
 echo 'top killed';
 sudo kill -2 $pid_perf;
 echo 'perf killed';
+echo '-----'
+echo 'done. check ./out/'$dir'
+echo '-----'
 exit
