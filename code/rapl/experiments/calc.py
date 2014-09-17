338f68adc0614c5ece4101b63baf98013252da37
gpestana <g6pestana@gmail.com>
2014-09-17 16:28:08 +0200

    acat paper

--- code/rapl/experiments/calc.py
+++ code/rapl/experiments/calc.py
@@ -5,6 +5,8 @@ import numpy as np
 filePath = sys.argv[1]
 data = utils.parse(filePath)
 
+#print data
+
 pck_avg = 0
 pp0_avg = 0
 dram_avg = 0
@@ -12,9 +14,12 @@ dram_avg = 0
 for core in range(0,32):
   PCK_NR = core/8
   
-  pck_avg = pck_avg + np.mean([float(i) for i in data[core]['pck']])
-  pp0_avg = pp0_avg + np.mean([float(i) for i in data[core]['pp0']])
-  dram_avg = dram_avg + np.mean([float(i) for i in data[core]['dram']])
+  if data[core]['pck'] > 0: 
+    pck_avg = pck_avg + np.mean([float(i) for i in data[core]['pck']])
+  if data[core]['pp0'] > 0: 
+    pp0_avg = pp0_avg + np.mean([float(i) for i in data[core]['pp0']])
+  if data[core]['dram'] > 0: 
+    dram_avg = dram_avg + np.mean([float(i) for i in data[core]['dram']])
 
 print pck_avg/32
 print pp0_avg/32
--- code/rapl/experiments/matPlot.py
+++ code/rapl/experiments/matPlot.py
@@ -21,21 +21,27 @@ for cpu in data:
     for i, val in enumerate(pck):
         if val < 0:
             pck[i] = last_val
-        last_val = pck[i]
+        else:
+          last_val = pck[i]
+          #print val
 
     pp0 = np.array(map(float, data[cpu_nr]['pp0']))
     for i, val in enumerate(pp0):
         if val < 0:
             pp0[i] = last_val
-        last_val = pp0[i]
+        else:
+          last_val = pp0[i]
 
 
     dram = np.array(map(float, data[cpu_nr]['dram']))
     for i, val in enumerate(dram):
         if val < 0:
             dram[i] = last_val
-        last_val = dram[i]
+        else:
+          last_val = dram[i]
 
+  
+    
 
     pck_total.append(np.mean(pck))
     pp0_total.append(np.mean(pp0))
@@ -43,24 +49,27 @@ for cpu in data:
 
     cpu_nr=cpu_nr+1
 
+print len(pck)
+print len(pp0)
+print len(dram)
 
 #whole mean:
 mean_pck = np.mean(pck_total)
 mean_pp0 = np.mean(pp0_total)
 mean_dram =  np.mean(dram_total)
-print mean_pck
-print mean_pp0
-print mean_dram
+print 'mean pck: '+str(mean_pck)
+print 'mean pp0: ' +str(mean_pp0)
+print 'mean dram: ' +str(mean_dram)
 
 #throughput:
 throughput = nr_events/time
 
 #throughput per watt:
-print 'pck'
+print 'throughput per watt pck'
 print throughput/mean_pck
 
-print 'pp0'
+print 'throughput per watt pp0'
 print throughput/mean_pp0
 
-print 'dram'
+print 'throughput per watt ram'
 print throughput/mean_dram
